9ec9fbe9cc2a3907b3e83700c902417ffc83e8a1
Shipiyaka <Alina.nagornaya49@gmail.com>
2023-02-17 17:29:23 +0100

    Added slider to gallery

---
+++ src/files/slider-gallery.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const slider_gallery = new ItcSimpleSlider("#slider-gallery", {
+    loop: true,
+    autoplay: false,
+    interval: 5000,
+    swipe: true,
+  });
+
+  document.querySelector(".btn-prev_gallery").onclick = function () {
+    slider_gallery.prev();
+  };
+  document.querySelector(".btn-next_gallery").onclick = function () {
+    slider_gallery.next();
+  };
+});
---
+++ src/files/slider-hero.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const slider_hero = new ItcSimpleSlider("#slider-hero", {
+    loop: true,
+    autoplay: false,
+    interval: 5000,
+    swipe: true,
+  });
+
+  document.querySelector(".btn-prev_hero").onclick = function () {
+    slider_hero.prev();
+  };
+  document.querySelector(".btn-next_hero").onclick = function () {
+    slider_hero.next();
+  };
+});
