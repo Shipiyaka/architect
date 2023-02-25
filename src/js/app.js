fd7d144d08a83f9b06ff2d10ca0e801163ff4347
Shipiyaka <Alina.nagornaya49@gmail.com>
2023-02-25 14:40:27 +0100

    Added popup to contact form

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,15 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
+$(".popup__show").click(function () {
+  var popup_id = $("#" + $(this).attr("rel"));
+  $(popup_id).show();
+});
+
+$(".popup__close").click(function () {
+  $(".popup").hide();
+});
+
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
